1a742976b30e5669e148662c85c9c3a6a47f33c2
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-07-12 20:41:15 -0500

    Naming de variables en Java

---
+++ HolaMundoJava/src/NamingJava.java
@@ -0,0 +1,35 @@
+             // Upper Camel Case
+public class NamingJava {
+
+                       //Lower Camel Case
+    public static void main(String[] args) {
+
+        // Utilizar convención Lower Camel Case para variables
+        // Todas las clases de Java empiezan en mayúsculas
+        int celphone = 323551651;
+        System.out.println(celphone);
+        
+        // Variables con símbolo de pesos o guion bajo
+        String $countryName = "Spain";
+        System.out.println("$countryName = " + $countryName);
+        String _backgroundColor = "Green";
+        System.out.println("_backgroundColor = " + _backgroundColor);
+        
+        // Tanto el símbolo de pesos como el guion bajo se puede
+        // intercalar en el nombre de la variable
+        String currency$ = "MXN";
+        System.out.println("currency$ = " + currency$);
+        String background_color = "Blue";
+        System.out.println("background_color = " + background_color);
+
+        // Convención para definición de constantes
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+        // Lower Camel Case
+        String fullName = "Irene Anahi Salgado Diaz";
+        int sizeInCentimeters = 26;
+
+    }
+}
