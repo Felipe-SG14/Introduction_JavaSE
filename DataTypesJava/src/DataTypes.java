2166551c469ba8e506e784783ab75ed84619ed76
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-07-12 20:49:29 -0500

    DataTypes in Java

---
+++ DataTypesJava/src/DataTypes.java
@@ -0,0 +1,34 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // Datos numéricos
+
+        // Enteros
+        /*
+        byte  -> -128 a 127
+        short -> -32768 a 32767
+        int   -> -2147483648 a 2147483647
+        long  ->
+         */
+        int n = 1234567890;
+        System.out.println("n = " + n);
+        // Agregar una L al final de un tipo de datos Long
+        long nL = 1234567890L;
+        System.out.println("nL = " + nL);
+
+
+        //Flotantes
+        /*
+        float
+        double
+         */
+
+        //Declarando double
+        double nD = 123.1516513;
+        System.out.println("nD = " + nD);
+        float  nF = 121365.61651f;
+        System.out.println("nF = " + nF);
+
+
+    }
+}
