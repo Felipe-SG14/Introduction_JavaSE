b4baff2459d56414049e59b57b2977bb307c1bca
Felipe-SG14 <felipe.solano.gos@gmail.com>
2022-07-13 20:09:43 -0500

    While Loop

--- HolaMundoJava/src/WhileLoop.java
+++ HolaMundoJava/src/WhileLoop.java
@@ -1,2 +1,26 @@
 public class WhileLoop {
+
+    public static boolean isTurnOnLight = false;
+
+    public static void main(String[] args){
+        turnOnOffLight();
+
+        int i = 0;
+        while(isTurnOnLight && i<=10){
+            printSOS();
+            i++;
+        }
+    }
+
+    public static void printSOS(){
+        System.out.println(". . . _ _ _ . . .");
+    }
+
+    public static boolean turnOnOffLight(){
+        // Operador ternario
+        // (expresión a evaluar) ? valor_true : valor_false;
+        isTurnOnLight = isTurnOnLight ? false : true;
+        return isTurnOnLight;
+    }
+
 }
